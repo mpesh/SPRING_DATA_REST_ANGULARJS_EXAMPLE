83c0be15f1b4fd09d759b16e278ba03a8d138786
m.peshawarwala <mufaddal.peshawarwala@newedge.com>
2014-10-14 11:48:22 -0500

    first commit

---
+++ src/main/java/com/mpexample/com/HomeController.java
@@ -0,0 +1,10 @@
+package com.mpexample.com;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class HomeController {
+    @RequestMapping("/home")
+    public String home() {
+        return "index1";
+    }
+}
